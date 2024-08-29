02a052dbccb90691d2332843707c3e8ab6c50db9
Adam <adam@MacBook-Pro-3.lan>
2024-08-28 22:04:13 -0400

    fixing impart canvas

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Projects from './components/Projects/Projects';
 import { useRef, useEffect } from 'react';
 import myGa from './myGa';
 import WSkills from './components/Skills/WSkills';
-import Canvas from './components/Skills/canvas';
 
 import { useSelector } from 'react-redux';
 
